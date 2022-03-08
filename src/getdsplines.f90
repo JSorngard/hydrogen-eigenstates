f3d8fc37bb3a3a6eb931809f53ca79bb2e910779
Johanna <jsorngard@gmail.com>
2022-03-08 14:34:31 +0100

    Removed non-standard variable kinds, improved whitespace

--- src/getdsplines.f90
+++ src/getdsplines.f90
@@ -1,68 +1,70 @@
 subroutine getdsplines(kntpts,n,k,x,out,o)
+    use iso_fortran_env
     implicit none
-    real*8,intent(in) :: x
-    integer,intent(in)::k,n
-    integer,intent(in),optional::o
-    real*8,dimension(n),intent(in)::kntpts
-    real*8,dimension(k),intent(inout)::out
-    integer::i,left,order
-    logical,external::isinf
+    real(real64), intent(in)                            :: x
+    integer,      intent(in)                            :: k, n
+    integer,      intent(in),   optional                :: o
+    real(real64), intent(in),           dimension(n)    :: kntpts
+    real(real64), intent(inout),        dimension(k)    :: out
+
+    integer :: i, left, order
+    logical, external :: isinf
 
 
-    left=0
+    left = 0
 
     if (present(o)) then
-        order=o
+        order = o
     else
-        order=1
+        order = 1
     endif
 
-    if (size(kntpts)<2*k+1) then
-        left=-1
-    elseif (n<2*k+1) then
-        left=-2
-    elseif(k<1)then
-        left=-3
-    elseif(x<kntpts(k))then
-        left=-4
-    elseif(size(out)/=k)then
-        left=-5
-    elseif(order<1)then
-        left=-6
+    if (size(kntpts) < 2*k + 1) then
+        left = -1
+    elseif (n < 2*k + 1) then
+        left = -2
+    elseif(k < 1)then
+        left = -3
+    elseif(x < kntpts(k))then
+        left = -4
+    elseif(size(out) /= k)then
+        left = -5
+    elseif(order < 1)then
+        left = -6
     endif
 
-    if (left<0) then
+    if (left < 0) then
         write(*,*) "getdsplines error: the input is invalid in argument",-left
     endif
 
-    do i=k,n-k+2 !Determine which knotpoint is the one directly to the left of x
-        if (kntpts(i)>x) then
-            left=i-1
+    do i = k, n-k+2 !Determine which knotpoint is the one directly to the left of x
+        if (kntpts(i) > x) then
+            left = i - 1
             exit
         endif
     enddo
 
-    if (i>n-k+1) then
+    if (i > n-k+1) then
         write(*,*) "getdsplines error: the knotpoint sequence has some problem (no point in the sequence is to the right of x)"
     endif
 
-    call dbsplvb(kntpts,n,k,1,x,left,out,order)
+    call dbsplvb(kntpts, n, k, 1, x, left, out, order)
     
-    if (x<kntpts(k).or.x>kntpts(size(kntpts))) then
-        do i=1,order
-            out(i)=0
+    if (x < kntpts(k) .or. x > kntpts(size(kntpts))) then
+        do i = 1, order
+            out(i) = 0.d0
         enddo
     endif
-    if (x<kntpts(k)) then
+    if (x < kntpts(k)) then
         write(*,*) "getdsplines error: requested splines outside the domain (left)"
         write(*,*) "at x=",x
-    elseif (x>kntpts(size(kntpts))) then
+    elseif (x > kntpts(size(kntpts))) then
         write(*,*) "getdsplines error: requested splines outside the domain (right)"
         write(*,*) "at x=",x
     endif
 
 
-     do i=1,size(out)
+     do i = 1, size(out)
         if(isinf(out(i))) then
             write(*,*) i,"getdsplines error: results from dbsplvb are infinity"
             exit
