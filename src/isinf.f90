e614f24ddd79de736393017ae2cf58f9f405c797
Johanna <jsorngard@gmail.com>
2022-03-08 14:20:07 +0100

    Removed non-standard variable kinds

--- src/isinf.f90
+++ src/isinf.f90
@@ -1,9 +1,12 @@
 function isinf(x)
+    use iso_fortran_env
     implicit none
-    real*8,intent(in) :: x
-    logical:: isinf
-    real*8::dbl_prec_var
-    real*8,parameter::infinity=huge(dbl_prec_var)
+    
+    real(real64), intent(in) :: x
+    
+    logical :: isinf
+    real(real64) :: dbl_prec_var
+    real(real64), parameter :: infinity=huge(dbl_prec_var)
 
-    isinf=((real(x,kind=8)>infinity).or.(real(x,kind=8))<-infinity)
+    isinf=((dble(x)>infinity).or.(dble(x))<-infinity)
 end function isinf
