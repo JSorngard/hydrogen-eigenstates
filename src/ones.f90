ad4755e6acd642f8a3ab2f10739b360fc38bf29b
Johanna <jsorngard@gmail.com>
2022-03-08 14:06:11 +0100

    Removed some unused files

--- src/ones.f90
+++
@@ -1,7 +0,0 @@
-subroutine ones(matrix,i,j)
-    implicit none
-    integer,intent(in)::i,j
-    real*8,intent(inout),dimension(i,j)::matrix
-
-    matrix(:,:)=real(1,kind=8)
-end subroutine ones
--- src/zeroes.f90
+++
@@ -1,7 +0,0 @@
-subroutine zeroes(matrix,i,j)
-    implicit none
-    integer,intent(in)::i,j
-    real*8,intent(inout),dimension(i,j)::matrix
-
-    matrix(:,:)=real(0,kind=8)
-end subroutine zeroes
