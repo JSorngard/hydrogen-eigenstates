31eac13272de64edd57e73b37b96f65c874f261d
Johanna <jsorngard@gmail.com>
2022-03-08 13:50:51 +0100

    Collected more expressions into one variable

--- src/eigensplines.f90
+++ src/eigensplines.f90
@@ -2,25 +2,29 @@ program eigensplines
     use iso_fortran_env
     implicit none
 
-    !Variables
+    !Physical constants
+    real(real64), parameter :: rb=5.29177211E-11, hplanck=6.62607E-34, me=9.109383E-31
+    real(real64), parameter :: pi=4*atan(1.d0), hbar=hplanck/(2*pi), epsilonnaught=8.854E-12, Q=1.6E-19
+    integer,  parameter :: Z=1
+    
+    !Variables, can be changed to change the system
     integer,  parameter :: l=0, nn=1
     integer,  parameter :: k=7, pts=50, out_unit=20
-    integer,  parameter :: neqs=pts-2*(k-1), nsplines=pts-k, n=3, nqpts=k+1
-    real(real64), parameter :: rb=5.29177211E-11, lstkntptSI=10*rb, lstkntpt=50., hplanck=6.62607E-34, me=9.109383E-31
-    real(real64), parameter :: pi=3.14159265359, hbar=hplanck/(2*pi), epsilonnaught=8.854E-12, Q=1.6E-19
-    integer,  parameter :: Z=1
+    integer,  parameter :: neqs=pts-2*(k-1), nsplines=pts-k, n=3, nqpts=k+1, syssize=nsplines-2
+    real(real64), parameter :: lstkntpt=50., lstkntptSI=10*rb
     logical,  parameter :: iprint=.false.
 
+    !Intermediate variables used for computation
     integer :: i, resolution
     real(real64), dimension(nqpts) :: abscissas, weights
     real(real64), external :: bspline, dbspline, glquad
     real(real64), dimension(pts) :: kntpts
-    real(real64), dimension(nsplines-2, nsplines-2) :: LHS, RHS
-    real(real64), dimension(nsplines, nsplines-2) :: nVR
-    real(real64), dimension(nsplines-2) :: ALPHAR, ALPHAI, BETA
-    real(real64), dimension(nsplines-2, nsplines-2) :: VL, VR
-    complex(real64), dimension(nsplines-2) :: eigens
-    integer, dimension(nsplines-2) :: ipiv
+    real(real64), dimension(syssize, syssize) :: LHS, RHS
+    real(real64), dimension(nsplines, syssize) :: nVR
+    real(real64), dimension(syssize) :: ALPHAR, ALPHAI, BETA
+    real(real64), dimension(syssize, syssize) :: VL, VR
+    complex(real64), dimension(syssize) :: eigens
+    integer, dimension(syssize) :: ipiv
     logical, external :: isinf
     integer, external :: splinestart, splineend
     real(real64) :: x
@@ -49,23 +53,24 @@ program eigensplines
         write(*,*) "Solving..."
     end if
 
-    call solve_eigensystem(LHS, RHS, nsplines, ALPHAR, ALPHAI, BETA, VL, VR, eigens)
+    call solve_eigensystem(LHS, RHS, syssize, ALPHAR, ALPHAI, BETA, VL, VR, eigens)
 
+    !Sort the eigenvalues by energy
     if(iprint) then
         write(*,*) "Sorting eigenvalues..."
     end if
-    do i = 1, size(ipiv)
+    do i = 1, syssize
         ipiv(i) = i
     enddo
-    call qsort(1, size(eigens), eigens, ipiv, size(eigens))
-    
+    call qsort(1, syssize, eigens, ipiv, syssize)
+    !Reorder the array of eigen vectors in the same way
     VR = VR(:,ipiv)
-    do i = 1, nsplines-2
+    do i = 1, syssize
         nVR(:,i) = (/0.d0, VR(:,i), 0.d0/)
     enddo
 
     write(*,*) "The negative eigenvalues are:"
-    do i = 1, size(eigens)
+    do i = 1, syssize
         if (dble(eigens(i)) < 0) then
             write(*,*) dble(eigens(i))
         endif
@@ -85,18 +90,16 @@ program eigensplines
 
 contains
 
-    subroutine solve_eigensystem(LHS,RHS,nsplines,ALPHAR,ALPHAI,BETA,VL,VR,eigens)
-        integer,         intent(in)                                     :: nsplines
-        real(real64),    intent(in),  dimension(nsplines-2, nsplines-2) :: LHS, RHS
-        real(real64),    intent(out), dimension(nsplines-2)             :: ALPHAR, ALPHAI, BETA
-        real(real64),    intent(out), dimension(nsplines-2, nsplines-2) :: VL, VR
-        complex(real64), intent(out), dimension(nsplines-2)             :: eigens
+    subroutine solve_eigensystem(LHS,RHS,syssize,ALPHAR,ALPHAI,BETA,VL,VR,eigens)
+        integer,         intent(in)                                 :: syssize
+        real(real64),    intent(in),  dimension(syssize, syssize)   :: LHS, RHS
+        real(real64),    intent(out), dimension(syssize)            :: ALPHAR, ALPHAI, BETA
+        real(real64),    intent(out), dimension(syssize, syssize)   :: VL, VR
+        complex(real64), intent(out), dimension(syssize)            :: eigens
 
         real(real64), dimension(3) :: array1
-        integer :: INFO, LWORK, syssize
+        integer :: INFO, LWORK
         real(real64), dimension(:), allocatable :: WORK
-
-        syssize = nsplines-2
 
         !Zeroing memory for lapack
         ALPHAR = 0.d0
@@ -122,11 +125,12 @@ contains
         call dggev('N','V',syssize,LHS,syssize,RHS,syssize,ALPHAR,ALPHAI,BETA,VL,syssize,VR,syssize,WORK,LWORK,INFO)
         
         if(INFO /= 0) then
-            write(*,*) "WARNING: LAPACK response code: ",INFO," when solving eigensystem. System size: ",syssize
+            write(*,*) "WARNING: LAPACK response code: ",INFO," when solving the system. System size: ",syssize
+            stop
         end if
 
         !Work out eigenvalues from lapack response
-        do i = 1, size(ALPHAR)
+        do i = 1, syssize
             eigens(i) = complex(ALPHAR(i)/BETA(i),ALPHAI(i)/BETA(i))
         enddo
     end subroutine solve_eigensystem
