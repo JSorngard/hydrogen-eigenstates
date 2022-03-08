007d19d51172271903462f68e18b4e2015d279c5
Johanna <jsorngard@gmail.com>
2022-03-08 14:42:45 +0100

    Removed non-standard variable kinds, improved whitespace

--- src/qsort.f90
+++ src/qsort.f90
@@ -9,81 +9,81 @@
 !size: the size of the input vectors (array1 and array2)
 !Author: Jimmy Vinbladh
 recursive subroutine qsort(left,right,array,index_vec,len_arr)
+    use iso_fortran_env
+    implicit none
 
-implicit none
+    integer,         intent(in)                         :: len_arr
+    integer,         intent(in)                         :: left, right
+    integer,         intent(inout), dimension(len_arr)  :: index_vec
+    complex(real64), intent(inout), dimension(len_arr)  :: array
 
-integer,intent(in)::len_arr
-integer,intent(in)::left,right
-integer,dimension(len_arr),intent(inout)::index_vec
-complex*16,dimension(len_arr), intent(inout)::array
+    integer :: bp, bl, br
 
-integer::bp,bl,br
-
-if (right .gt. left)then
-    call quick_sort(left,right,array,index_vec,len_arr,bp)
-    br = bp-1
-    bl = bp+1
-    call qsort(left,br,array,index_vec,len_arr)
-    call qsort(bl,right,array,index_vec,len_arr)
-end if
-
+    if (right .gt. left)then
+        call quick_sort(left, right, array, index_vec, len_arr, bp)
+        br = bp - 1
+        bl = bp + 1
+        call qsort(left, br, array, index_vec, len_arr)
+        call qsort(bl, right, array, index_vec, len_arr)
+    end if
 end subroutine qsort
 
 subroutine quick_sort(leftt,rightt,array,index_vec,len_arr,bp)
-implicit none
+    use iso_fortran_env
+    implicit none
 
-integer,intent(in)::len_arr
-integer,intent(in)::leftt,rightt
-integer,dimension(len_arr),intent(inout)::index_vec
-complex*16,dimension(len_arr), intent(inout)::array
-integer,intent(out)::bp
+    integer,         intent(in)                         :: len_arr
+    integer,         intent(in)                         :: leftt, rightt
+    integer,         intent(inout), dimension(len_arr)  :: index_vec
+    complex(real64), intent(inout), dimension(len_arr)  :: array
+    integer,         intent(out)                        :: bp
 
-integer::p,org_right,temp_ind, piv_ind,left,right
-complex*16::temp_el,piv
+    integer :: p, org_right, temp_ind, piv_ind, left, right
+    complex(real64) :: temp_el, piv
 
-left = leftt
-right = rightt
-
-
-p = floor((left+right)/2.d0)
-piv         = array(p)
-array(p)    = array(right)
-piv_ind     = index_vec(p)
-index_vec(p)= index_vec(right)
-org_right   = right
-
-do while (left .lt. right)
-    right = right-1
-	do while (real(array(left)) .le. real(piv) .and. left .lt. right)
-        left = left+1
-	end do
-	do while (real(array(right)) .gt. real(piv) .and. right .gt. left)
-        right = right-1
-	end do
-	if (real(array(left)) .gt. real(array(right)))then
-        temp_el         = array(right)
-        temp_ind        = index_vec(right)
-        array(right)    = array(left)
-        index_vec(right)= index_vec(left)
-        array(left)     = temp_el
-        index_vec(left) = temp_ind
-	end if
-end do
+    left = leftt
+    right = rightt
 
 
-if (real(array(right)) .gt. real(piv)) then
-    array(org_right)    = array(right)
-    index_vec(org_right)= index_vec(right)
-    array(right)        = piv
-    index_vec(right)    = piv_ind
-    bp                  = right
-else
-    array(org_right)    = array(right+1)
-    index_vec(org_right)= index_vec(right+1)
-    array(right+1)      = piv
-    index_vec(right+1)  = piv_ind
-    bp                  = right+1
-end if
+    p = floor((left + right)/2.d0)
+    piv          = array(p)
+    array(p)     = array(right)
+    piv_ind      = index_vec(p)
+    index_vec(p) = index_vec(right)
+    org_right    = right
+
+    do while (left .lt. right)
+        right = right - 1
+    	do while (real(array(left)) .le. real(piv) .and. left .lt. right)
+            left = left + 1
+    	end do
+    	do while (real(array(right)) .gt. real(piv) .and. right .gt. left)
+            right = right - 1
+    	end do
+    	if (real(array(left)) .gt. real(array(right)))then
+            temp_el          = array(right)
+            temp_ind         = index_vec(right)
+            array(right)     = array(left)
+            index_vec(right) = index_vec(left)
+            array(left)      = temp_el
+            index_vec(left)  = temp_ind
+    	end if
+    end do
+
+
+    if (real(array(right)) .gt. real(piv)) then
+        array(org_right)     = array(right)
+        index_vec(org_right) = index_vec(right)
+        array(right)         = piv
+        index_vec(right)     = piv_ind
+        bp                   = right
+    else
+        array(org_right)     = array(right+1)
+        index_vec(org_right) = index_vec(right+1)
+        array(right+1)       = piv
+        index_vec(right+1)   = piv_ind
+        bp                   = right + 1
+    end if
 
 end subroutine quick_sort
 
